3a3c06012b12e4a5d685972d124559e71663068d
Chris Dzombak <chris@chrisdzombak.net>
2013-11-05 23:21:41 -0500

    Use event page instead of background page

--- extension/openlist-bg.js
+++ extension/openlist-bg.js
@@ -1,3 +1,5 @@
-chrome.contextMenus.create({"title": "OpenList", "contexts":["selection"], "onclick": function(info, tab) {
+chrome.contextMenus.create({"title": "OpenList", "id": "OpenList-ContextMenuOpen", "contexts":["selection"]});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
     openList(info.selectionText);
-}});
+})
